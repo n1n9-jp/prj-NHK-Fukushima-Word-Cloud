2f67a489ca01983a03492a475e586adcadacd3b6
Yuichi Yazaki <yuichi@n1n9.jp>
2015-12-03 20:01:20 +0900

    d3.layout.cloud.js導入
    タブ切り替えに対応した形で、d3.layout.cloud.jsを導入

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,10 +55,13 @@ $(document).ready(function(){
 	/* ---------------
 	Initialize
 	--------------- */
-	d3.select("#container1").style("opacity", 0.0);
-	d3.select("#container2").style("opacity", 0.0);
-	d3.select("#container3").style("opacity", 0.0);
+	// d3.select("#container1").style("opacity", 0.0);
+	// d3.select("#container2").style("opacity", 0.0);
+	// d3.select("#container3").style("opacity", 0.0);
 
+	var vis = new Array(); var vid = 0;
+    var fontSizeScale = d3.scale['sqrt']().range([14, 180]);
+    var layout1, layout2, layout3;
 
 	/* ---------------
 	Viewport
@@ -68,14 +71,137 @@ $(document).ready(function(){
 	    width  = bWidth  - margin.left - margin.right,
 	    height = bHeight - margin.top - margin.bottom;
 
-	var svgContainer = d3.select("#svgContainer").append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
+	// var svgContainer = d3.select("#svgContainer").append("svg")
+	//     .attr("width", width + margin.left + margin.right)
+	//     .attr("height", height + margin.top + margin.bottom)
+	//     .attr("viewBox", "0 0 1200 500")
+	//     .attr("preserveAspectRatio", "xMidYMid")
+	//     .attr("id", "chartArea")
+	//   .append("g")
+	//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+
+
+var widthArray = new Array();
+widthArray[0] = 1200;
+widthArray[1] = 400;
+widthArray[2] = 400;
+widthArray[3] = 400;
+widthArray[4] = 400;
+widthArray[5] = 400;
+widthArray[6] = 400;
+widthArray[7] = 600;
+widthArray[8] = 600;
+
+
+	/* all */
+	vis[0] = d3.select("#svgcontainer0").append("svg")
+	    .attr("width", widthArray[0])
+	    .attr("height", height)
 	    .attr("viewBox", "0 0 1200 500")
 	    .attr("preserveAspectRatio", "xMidYMid")
-	    .attr("id", "chartArea")
+	    .attr("id", "chartArea0")
 	  .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	    .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+
+
+
+
+	/* age */
+	vis[1] = d3.select("#svgcontainer1").append("svg")
+	    .attr("width", widthArray[1])
+	    .attr("height", height)
+	    // .attr("viewBox", "0 0 400 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea1")
+	  .append("g")
+	     .attr("transform", "translate(" + widthArray[1]/2 + "," + height/2 + ")");
+
+	vis[2] = d3.select("#svgcontainer2").append("svg")
+	    .attr("width", widthArray[2])
+	    .attr("height", height)
+	    .attr("transform", "translate(" + widthArray[2] + "," + height/2 + ")")
+	    // .attr("viewBox", "0 0 400 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea2")
+	  .append("g")
+	     .attr("transform", "translate(" + widthArray[2]/2 + "," + height/2 + ")");
+
+	vis[3] = d3.select("#svgcontainer3").append("svg")
+	    .attr("width", widthArray[3])
+	    .attr("height", height)
+	    .attr("transform", "translate(" + widthArray[3]*2 + "," + height/2 + ")")
+	    // .attr("viewBox", "0 0 400 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea3")
+	  .append("g")
+	     .attr("transform", "translate(" + widthArray[3]/2 + "," + height/2 + ")");
+
+
+
+
+
+
+	/* area */
+	vis[4] = d3.select("#svgcontainer4").append("svg")
+	    .attr("width", widthArray[4])
+	    .attr("height", height)
+	    .attr("viewBox", "0 0 400 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea4")
+	  .append("g")
+	    .attr("transform", "translate(" + widthArray[4]/2 + "," + height/2 + ")");
+
+	vis[5] = d3.select("#svgcontainer5").append("svg")
+	    .attr("width", widthArray[5])
+	    .attr("height", height)
+	    .attr("transform", "translate(" + widthArray[5] + "," + height/2 + ")")
+	    .attr("viewBox", "0 0 400 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea5")
+	  .append("g")
+	    .attr("transform", "translate(" + widthArray[5]/2 + "," + height/2 + ")");
+
+	vis[6] = d3.select("#svgcontainer6").append("svg")
+	    .attr("width", widthArray[6])
+	    .attr("height", height)
+	    .attr("transform", "translate(" + widthArray[6]*2 + "," + height/2 + ")")
+	    .attr("viewBox", "0 0 400 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea6")
+	  .append("g")
+	    .attr("transform", "translate(" + widthArray[6]/2 + "," + height/2 + ")");
+
+
+
+
+	/* gender */
+	vis[7] = d3.select("#svgcontainer7").append("svg")
+	    .attr("width", widthArray[7])
+	    .attr("height", height)
+	    .attr("viewBox", "0 0 600 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea7")
+	  .append("g")
+	    .attr("transform", "translate(" + widthArray[7]/2 + "," + height/2 + ")");
+
+	vis[8] = d3.select("#svgcontainer8").append("svg")
+	    .attr("width", widthArray[8])
+	    .attr("height", height)
+	    .attr("transform", "translate(" + widthArray[8] + "," + height/2 + ")")
+	    .attr("viewBox", "0 0 600 500")
+	    .attr("preserveAspectRatio", "xMidYMid")
+	    .attr("id", "chartArea8")
+	  .append("g")
+	    .attr("transform", "translate(" + widthArray[8]/2 + "," + height/2 + ")");
+
+
+
+
+
 
 
 	var Graph = function() {
@@ -83,9 +209,16 @@ $(document).ready(function(){
 	    var self = this;
 	    this.e = new Eventer;
 
+    	var tags = new Array();
+
+	    var grayScale = d3.scale.linear()
+	      .domain([1, 80])
+	      .range(["#BBB", "#FFF"]);
 
 	    this.init = function() {
 	        this.e.subscribe( 'load', this.getData );
+	        this.e.subscribe( 'draw:viewport', this.drawViewport );
+        	this.e.subscribe( 'draw:update', this.drawUpdate );
 	        this.e.subscribe( 'container:disappear', this.disappearContainer );
 	        this.e.subscribe( 'container:appear', this.appearContainer );
 
@@ -93,11 +226,189 @@ $(document).ready(function(){
 	    };
 
 
+		this.getData = function() {
+
+		    queue()
+		        .defer(d3.tsv, "assets/data/all.tsv")
+		        .defer(d3.tsv, "assets/data/age203040.tsv")
+		        .defer(d3.tsv, "assets/data/age5060.tsv")
+		        .defer(d3.tsv, "assets/data/age70.tsv")
+		        .defer(d3.tsv, "assets/data/area12.tsv")
+		        .defer(d3.tsv, "assets/data/area3.tsv")
+		        .defer(d3.tsv, "assets/data/area4.tsv")
+		        .defer(d3.tsv, "assets/data/genderMale.tsv")
+		        .defer(d3.tsv, "assets/data/genderFemale.tsv")
+		        .await(loadReady);
+
+
+		    function loadReady(_error, _all, _age203040, 
+		    					_age5060, _age70, _area12, _area3, _area4, 
+		    					_genderMale, _genderFemale)
+		    {
+		    	tags[0] = $.extend(true, [], _all);
+		    	tags[1] = $.extend(true, [], _age203040);
+		    	tags[2] = $.extend(true, [], _age5060);
+		    	tags[3] = $.extend(true, [], _age70);
+		    	tags[4] = $.extend(true, [], _area12);
+		    	tags[5] = $.extend(true, [], _area3);
+		    	tags[6] = $.extend(true, [], _area4);    	
+		    	tags[7] = $.extend(true, [], _genderMale);
+		    	tags[8] = $.extend(true, [], _genderFemale);
+
+            	self.e.publish('draw:viewport');
+		    }
+
+		}
+
+
+	    this.drawViewport = function() {
+
+	    	console.log("drawViewport");
+
+	        layout1 = d3.layout.cloud()
+	                .timeInterval(Infinity)
+	                .size([width, height])
+	                .fontSize(function(d) {
+	                    return fontSizeScale(+d.value);
+	                })
+	                .text(function(d) {
+	                	//console.log("key", d.key);
+	                    return d.key;
+	                })
+	                .on("end", draw);
+	        layout1.font('YuGothic').spiral('archimedean');
+
+
+	        layout2 = d3.layout.cloud()
+	                .timeInterval(Infinity)
+	                .size([width/2, height])
+	                .fontSize(function(d) {
+	                    return fontSizeScale(+d.value);
+	                })
+	                .text(function(d) {
+	                	//console.log("key", d.key);
+	                    return d.key;
+	                })
+	                .on("end", draw);
+	        layout2.font('YuGothic').spiral('archimedean');
+
+
+	        layout3 = d3.layout.cloud()
+	                .timeInterval(Infinity)
+	                .size([width/3, height])
+	                .fontSize(function(d) {
+	                    return fontSizeScale(+d.value);
+	                })
+	                .text(function(d) {
+	                	//console.log("key", d.key);
+	                    return d.key;
+	                })
+	                .on("end", draw);
+	        layout3.font('YuGothic').spiral('archimedean');
+
+
+
+	        fontSizeScale.domain([10, 255])
+
+	        self.e.publish('draw:update');
+
+	    };
+
+
+	    this.drawUpdate = function() {
+	    	console.log("drawUpdate"); // x9
+
+	    	if (vid == 0) {
+	        	layout1.stop().words( tags[vid] ).start();
+	    	} else if ((vid == 7) || (vid == 8)) {
+	        	layout2.stop().words( tags[vid] ).start();
+	    	} else {
+	        	layout3.stop().words( tags[vid] ).start();	
+	    	}
+
+	    };
+
+
+
+	    function draw(data, bounds) {
+
+	    	console.log("bounds", bounds);
+	    	var _width = widthArray[vid];
+
+
+	    	console.log("vid", vid);
+	    	console.log("_width", _width);
+
+
+	        vis[vid].attr("width", _width).attr("height", height);
+
+	        _scale = bounds ? Math.min(
+	                _width / Math.abs(bounds[1].x - _width / 2),
+	                _width / Math.abs(bounds[0].x - _width / 2),
+	                height / Math.abs(bounds[1].y - height / 2),
+	                height / Math.abs(bounds[0].y - height / 2)) / 2 : 1;
+
+	        var text = vis[vid].selectAll("text")
+	                .data(data, function(d) {
+	                    return d.text.toLowerCase();
+	                });
+	                
+	        text.transition()
+	                .duration(1000)
+	                .attr("transform", function(d) {
+	                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+	                })
+	                .style("font-size", function(d) {
+	                    return d.size + "px";
+	                });;
+
+	        text.enter().append("text")
+	                .attr("text-anchor", "middle")
+	                .attr("transform", function(d) {
+	                    return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+	                })
+	                .style("font-size", function(d) {
+	                    return d.size + "px";
+	                })
+	                //.style("opacity", 1e-6)
+	                .transition()
+	                .duration(1000);
+
+
+	        text.style("font-family", function(d) {
+	                    return d.font;
+	                })
+	                .style("fill", function(d,i) {
+	                    return grayScale( +d.value );
+	                })
+	                .style("opacity", 1.0)
+	                .text(function(d) {
+	                    return d.text;
+	                })
+	                .on("mouseover", function (d, i){
+	                    //console.log("mouseover");
+	                })
+	                .on("mouseout", function (d, opacity){
+	                    //console.log("mouseout");
+	                })
+	                .on("click", function (d, i){
+	                    //makeModalDialog();
+	                });
+
+
+	        vid++;
+	        if (vid<9) {
+	            self.e.publish('draw:update');
+	        }
+	    }
+
+
+
 		/* ---------------
 		navigation
 		--------------- */
 		//var menuNum = 4;
-	    var currentNum = 0, prevNum = -1;
+	    var currentNum = 0, prevNum = 0;
 
 		var menuItems = d3.select("#radioBlock").append('form').selectAll("span")
 		    .data( ["全体", "年齢", "区域", "性別"] )
@@ -116,7 +427,7 @@ $(document).ready(function(){
 			.attr('value', function(d, i) {
 				return d;
 			})
-		    .property("checked", function(d, i) { 
+		    .property("checked", function(d, i) {
 			    if (i === prevNum) { return true; } else { return false; };
 		    })
 			.on("change", function(d,i){
@@ -143,15 +454,19 @@ $(document).ready(function(){
 			console.log("prevNum", prevNum);
 			console.log("currentNum", currentNum);
 
-			d3.select("#container" + prevNum).style("opacity", 0.0);
-
-		    self.e.publish('container:appear');
+			// $("#container" + prevNum).fadeOut('slow', function() {
+			// });
+			$("#container" + prevNum).animate( { opacity: 'hide'}, { duration: 1000, easing: 'swing'} );
+			self.e.publish('container:appear');
+		    
 		}
 
 
 		this.appearContainer = function() {
 
-			d3.select("#container" + currentNum).style("opacity", 1.0);
+			console.log("here.");
+			$("#container" + currentNum).animate( { opacity: 'show'}, { duration: 1000, easing: 'swing'} );
+			//d3.select("#container" + currentNum).style("opacity", 1.0);
 		}
 
 
@@ -166,15 +481,36 @@ $(document).ready(function(){
 /* ---------------
 utility: window resize
 --------------- */
+var _chart0 = $("#chartArea0");
+var _chart1 = $("#chartArea1");
+var _chart2 = $("#chartArea2");
+var _chart3 = $("#chartArea3");
+var _chart4 = $("#chartArea4");
+var _chart5 = $("#chartArea5");
+var _chart6 = $("#chartArea6");
+var _chart7 = $("#chartArea7");
+var _chart8 = $("#chartArea8");
+
 var _chart = $("#chartArea"),
     aspect = _chart.width() / _chart.height(),
     container = _chart.parent();
+
 $(window).on("resize", function() {
     console.log("resized");
     var targetWidth = container.width();
-    width = targetWidth;
-    height = Math.round(targetWidth / aspect);
-    _chart.attr("width", width);
-    _chart.attr("height", height);
+    var _w = targetWidth;
+    var _h = Math.round(targetWidth / aspect);
+
+    _chart.attr("width", _w); _chart.attr("height", _h);
+    // _chart0.attr("width", _w); _chart0.attr("height", _h);
+    // _chart1.attr("width", _w); _chart1.attr("height", _h);
+    // _chart2.attr("width", _w); _chart2.attr("height", _h);
+    // _chart3.attr("width", _w); _chart3.attr("height", _h);
+    // _chart4.attr("width", _w); _chart4.attr("height", _h);
+    // _chart5.attr("width", _w); _chart5.attr("height", _h);
+    // _chart6.attr("width", _w); _chart6.attr("height", _h);
+    // _chart7.attr("width", _w); _chart7.attr("height", _h);
+    // _chart8.attr("width", _w); _chart8.attr("height", _h);
+
 
 }).trigger("resize");
